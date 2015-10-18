618183fefacaf6bf965ffc4bfb3f625175dc94d5
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2015-10-19 01:52:56 +0530

    tools: adapt roomservice for MinimalOS
    Signed-off-by: Chinmay Kunkikar <chinmay.kunkikar@gmail.com>

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -36,14 +36,14 @@ except ImportError:
 # set this to the default remote to use in repo
 default_rem = "github"
 # set this to the default revision to use (branch/tag name)
-default_rev = "android-4.3"
+default_rev = "mm-6.0"
 # set this to the remote that you use for projects from your team repos
-# example fetch="https://github.com/omnirom"
-default_team_rem = "omnirom"
+# example fetch="https://github.com/MinimalOS-AOSP"
+default_team_rem = "github"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "omnirom"
+android_team = "MinimalOS-AOSP"
 
 
 def check_repo_exists(git_data):
@@ -207,7 +207,7 @@ def parse_device_from_folder(device):
 
 
 def parse_dependency_file(location):
-    dep_file = "omni.dependencies"
+    dep_file = "minimal.dependencies"
     dep_location = '/'.join([location, dep_file])
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
